ffb7fda566069fa392c8f9d5729cfc0b959c06d2
Thomas Durand <Dean151@users.noreply.github.com>
2022-12-01 08:53:41 +0100

    Initial commit

---
+++ Sources/AoC/Puzzle+Execute.swift
@@ -0,0 +1,58 @@
+//
+//  Puzzle+Execute.swift
+//  AoC-Swift-Template
+//  Forked from https://github.com/Dean151/AoC-Swift-Template
+//
+//  Created by Thomas DURAND.
+//  Follow me on Twitter @deanatoire
+//  Check my computing blog on https://www.thomasdurand.fr/
+//
+
+import Foundation
+
+public enum ExecutionError: Error {
+    case notSolved
+    case unsolvable
+}
+
+extension Puzzle {
+    public static func main() async {
+        do {
+            // Input resolution
+            let rawInput = try await rawInput()
+            let input = try await transform(raw: rawInput)
+
+            let start = DispatchTime.now()
+
+            // Part 1
+            let part1 = try await solvePartOne(input)
+            print("\(self.self)-1 has been solved!")
+            print(part1)
+
+            printSeparator()
+
+            // Part 2
+            let part2 = try await solvePartTwo(input)
+            print("\(self.self)-2 has been solved!")
+            print(part2)
+
+            printSeparator()
+            printElapsedTime(from: start)
+        } catch {
+            print("Execution raised an error: \(error)")
+            exit(1)
+        }
+    }
+
+    static func printElapsedTime(from start: DispatchTime) {
+        let elapsed = Double(DispatchTime.now().uptimeNanoseconds - start.uptimeNanoseconds) / 1_000_000_000
+        let formatter = NumberFormatter()
+        formatter.minimumIntegerDigits = 1
+        formatter.maximumFractionDigits = 3
+        print("Elapsed time: \(formatter.string(from: NSNumber(value: elapsed))!)s")
+    }
+
+    static func printSeparator() {
+        print("––––––––––")
+    }
+}
---
+++ Sources/AoC/Puzzle+Input.swift
@@ -0,0 +1,78 @@
+//
+//  Puzzle+Input.swift
+//  AoC-Swift-Template
+//  Forked from https://github.com/Dean151/AoC-Swift-Template
+//
+//  Created by Thomas DURAND.
+//  Follow me on Twitter @deanatoire
+//  Check my computing blog on https://www.thomasdurand.fr/
+//
+
+import Foundation
+
+public enum InputError: Error {
+    case unexpectedInput
+}
+
+extension Puzzle {
+    public static var componentsSeparator: CharacterSet? { .newlines }
+
+    static var inputFile: URL {
+        Bundle.main.bundleURL.appending(path: "AdventOfCode_\(self.self).bundle/Contents/Resources/input.txt")
+    }
+
+    /// Get the input file content as a unique string
+    public static func rawInput() throws -> String {
+        try String(contentsOf: inputFile).trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+}
+
+// MARK: Parsable
+
+public protocol Parsable {
+    static func parse(raw: String) throws -> Self
+}
+
+extension Puzzle where Input: Parsable {
+    public static func transform(raw: String) throws -> Input {
+        try .parse(raw: raw)
+    }
+}
+
+// Thanks @franklefebvre for this generic constraint syntax :)
+extension Puzzle where Input: Sequence, Input.Element: Parsable {
+    public static func transform(raw: String) throws -> [Input.Element] {
+        guard let componentsSeparator else {
+            return try raw.map({ char in
+                try .parse(raw: String(char))
+            })
+        }
+        return try raw.components(separatedBy: componentsSeparator).map {
+            try .parse(raw: $0)
+        }
+    }
+}
+
+extension String: Parsable {
+    public static func parse(raw: String) throws -> String {
+        return raw
+    }
+}
+
+extension Int: Parsable {
+    public static func parse(raw: String) throws -> Int {
+        guard let integer = Int(raw) else {
+            throw InputError.unexpectedInput
+        }
+        return integer
+    }
+}
+
+extension Double: Parsable {
+    public static func parse(raw: String) throws -> Double {
+        guard let double = Double(raw) else {
+            throw InputError.unexpectedInput
+        }
+        return double
+    }
+}
