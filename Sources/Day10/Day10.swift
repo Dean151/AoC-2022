e24436f10ca1807f78315bd62002e729014bf0f0
Thomas Durand <thomas.durand.verjat@gmail.com>
2022-12-10 12:38:26 +0100

    Solved day 10

--- Sources/Day10/Day10.swift
+++ Sources/Day10/Day10.swift
@@ -15,9 +15,51 @@ import Common
 
 @main
 struct Day10: Puzzle {
-    typealias Input = String
-    typealias OutputPartOne = Never
-    typealias OutputPartTwo = Never
+    typealias Input = [Command]
+    typealias OutputPartOne = Int
+    typealias OutputPartTwo = Void
+}
+
+enum Command: Parsable {
+    case noop
+    case addx(value: Int)
+
+    static func parse(raw: String) throws -> Command {
+        let components = raw.components(separatedBy: .whitespaces)
+        if components[0] == "noop", components.count == 1 {
+            return .noop
+        }
+        guard components.count == 2, components[0] == "addx", let value = Int(components[1]) else {
+            throw InputError.unexpectedInput(unrecognized: raw)
+        }
+        return .addx(value: value)
+    }
+}
+
+enum CPU {
+     static func execute(program commands: [Command], duringCycle: (Int, Int) -> Void) {
+         var register = 1
+         var cycle = 0
+
+         func performCycle() {
+             // Starting a cycle
+             cycle += 1
+             // During the cycle
+             duringCycle(cycle, register)
+             // Ending the cycle
+         }
+
+         for command in commands {
+             switch command {
+             case .noop:
+                 performCycle()
+             case .addx(value: let value):
+                 performCycle()
+                 performCycle()
+                 register += value
+             }
+         }
+    }
 }
 
 // MARK: - PART 1
@@ -25,13 +67,20 @@ struct Day10: Puzzle {
 extension Day10 {
     static var partOneExpectations: [any Expectation<Input, OutputPartOne>] {
         [
-            // TODO: add expectations for part 1
+            assert(expectation: 13140, fromRaw: "addx 15\naddx -11\naddx 6\naddx -3\naddx 5\naddx -1\naddx -8\naddx 13\naddx 4\nnoop\naddx -1\naddx 5\naddx -1\naddx 5\naddx -1\naddx 5\naddx -1\naddx 5\naddx -1\naddx -35\naddx 1\naddx 24\naddx -19\naddx 1\naddx 16\naddx -11\nnoop\nnoop\naddx 21\naddx -15\nnoop\nnoop\naddx -3\naddx 9\naddx 1\naddx -3\naddx 8\naddx 1\naddx 5\nnoop\nnoop\nnoop\nnoop\nnoop\naddx -36\nnoop\naddx 1\naddx 7\nnoop\nnoop\nnoop\naddx 2\naddx 6\nnoop\nnoop\nnoop\nnoop\nnoop\naddx 1\nnoop\nnoop\naddx 7\naddx 1\nnoop\naddx -13\naddx 13\naddx 7\nnoop\naddx 1\naddx -33\nnoop\nnoop\nnoop\naddx 2\nnoop\nnoop\nnoop\naddx 8\nnoop\naddx -1\naddx 2\naddx 1\nnoop\naddx 17\naddx -9\naddx 1\naddx 1\naddx -3\naddx 11\nnoop\nnoop\naddx 1\nnoop\naddx 1\nnoop\nnoop\naddx -13\naddx -19\naddx 1\naddx 3\naddx 26\naddx -30\naddx 12\naddx -1\naddx 3\naddx 1\nnoop\nnoop\nnoop\naddx -9\naddx 18\naddx 1\naddx 2\nnoop\nnoop\naddx 9\nnoop\nnoop\nnoop\naddx -1\naddx 2\naddx -37\naddx 1\naddx 3\nnoop\naddx 15\naddx -21\naddx 22\naddx -6\naddx 1\nnoop\naddx 2\naddx 1\nnoop\naddx -10\nnoop\nnoop\naddx 20\naddx 1\naddx 2\naddx 2\naddx -6\naddx -11\nnoop\nnoop\nnoop")
         ]
     }
 
     static func solvePartOne(_ input: Input) async throws -> OutputPartOne {
-        // TODO: Solve part 1 :)
-        throw ExecutionError.notSolved
+        var signalStrengthSum = 0
+        let cyclesOfInterest: Set<Int> = [20, 60, 100, 140, 180, 220]
+        CPU.execute(program: input) { cycle, register in
+            guard cyclesOfInterest.contains(cycle) else {
+                return
+            }
+            signalStrengthSum += cycle * register
+        }
+        return signalStrengthSum
     }
 }
 
@@ -40,12 +89,20 @@ extension Day10 {
 extension Day10 {
     static var partTwoExpectations: [any Expectation<Input, OutputPartTwo>] {
         [
-            // TODO: add expectations for part 2
+            assert(expectation: (), fromRaw: "addx 15\naddx -11\naddx 6\naddx -3\naddx 5\naddx -1\naddx -8\naddx 13\naddx 4\nnoop\naddx -1\naddx 5\naddx -1\naddx 5\naddx -1\naddx 5\naddx -1\naddx 5\naddx -1\naddx -35\naddx 1\naddx 24\naddx -19\naddx 1\naddx 16\naddx -11\nnoop\nnoop\naddx 21\naddx -15\nnoop\nnoop\naddx -3\naddx 9\naddx 1\naddx -3\naddx 8\naddx 1\naddx 5\nnoop\nnoop\nnoop\nnoop\nnoop\naddx -36\nnoop\naddx 1\naddx 7\nnoop\nnoop\nnoop\naddx 2\naddx 6\nnoop\nnoop\nnoop\nnoop\nnoop\naddx 1\nnoop\nnoop\naddx 7\naddx 1\nnoop\naddx -13\naddx 13\naddx 7\nnoop\naddx 1\naddx -33\nnoop\nnoop\nnoop\naddx 2\nnoop\nnoop\nnoop\naddx 8\nnoop\naddx -1\naddx 2\naddx 1\nnoop\naddx 17\naddx -9\naddx 1\naddx 1\naddx -3\naddx 11\nnoop\nnoop\naddx 1\nnoop\naddx 1\nnoop\nnoop\naddx -13\naddx -19\naddx 1\naddx 3\naddx 26\naddx -30\naddx 12\naddx -1\naddx 3\naddx 1\nnoop\nnoop\nnoop\naddx -9\naddx 18\naddx 1\naddx 2\nnoop\nnoop\naddx 9\nnoop\nnoop\nnoop\naddx -1\naddx 2\naddx -37\naddx 1\naddx 3\nnoop\naddx 15\naddx -21\naddx 22\naddx -6\naddx 1\nnoop\naddx 2\naddx 1\nnoop\naddx -10\nnoop\nnoop\naddx 20\naddx 1\naddx 2\naddx 2\naddx -6\naddx -11\nnoop\nnoop\nnoop")
         ]
     }
 
     static func solvePartTwo(_ input: Input) async throws -> OutputPartTwo {
-        // TODO: Solve part 2 :)
-        throw ExecutionError.notSolved
+        CPU.execute(program: input) { cycle, register in
+            if abs(((cycle-1) % 40) - register) <= 1 {
+                print("#", terminator: "")
+            } else {
+                print(" ", terminator: "")
+            }
+            if cycle % 40 == 0 {
+                print("", terminator: "\n")
+            }
+        }
     }
 }
