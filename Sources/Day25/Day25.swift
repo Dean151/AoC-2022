98e817c5f2c4309c9cc48c470e82bf8c916e6791
Thomas Durand <thomas.durand.verjat@gmail.com>
2022-12-25 11:35:43 +0100

    Started day 25 ; not enough start for part 2 :'(

--- Sources/Day25/Day25.swift
+++ Sources/Day25/Day25.swift
@@ -15,9 +15,93 @@ import Common
 
 @main
 struct Day25: Puzzle {
-    typealias Input = String
-    typealias OutputPartOne = Never
+    typealias Input = [SnafuNumber]
+    typealias OutputPartOne = String
     typealias OutputPartTwo = Never
+}
+
+struct SnafuNumber: Parsable, CustomStringConvertible, ExpressibleByIntegerLiteral {
+    enum Digit: Int, CustomStringConvertible {
+        case double = 2, simple = 1, zero = 0, minus = -1, doubleMinus = -2
+
+        var description: String {
+            switch self {
+            case .double:
+                return "2"
+            case .simple:
+                return "1"
+            case .zero:
+                return "0"
+            case .minus:
+                return "-"
+            case .doubleMinus:
+                return "="
+            }
+        }
+
+        static func parse(raw: Character) throws -> SnafuNumber.Digit {
+            switch raw {
+            case "2":
+                return .double
+            case "1":
+                return .simple
+            case "0":
+                return .zero
+            case "-":
+                return .minus
+            case "=":
+                return .doubleMinus
+            default:
+                throw InputError.unexpectedInput(unrecognized: String(raw))
+            }
+        }
+    }
+
+    var digits: [Digit]
+
+    var description: String {
+        var description = ""
+        for digit in digits.reversed() {
+            description += digit.description
+        }
+        return description
+    }
+
+    var intValue: Int {
+        var value = 0
+        for (power, amount) in digits.enumerated() {
+            value += Int(pow(Double(5), Double(power))) * amount.rawValue
+        }
+        return value
+    }
+
+    init(digits: [Digit]) {
+        self.digits = digits
+    }
+
+    init(integerLiteral value: Int) {
+        var digits: [Digit] = []
+        var number = value
+        while number != 0 {
+            let rest = number % 5
+            if rest > 2 {
+                number += rest
+                digits.append(.init(rawValue: rest - 5)!)
+            } else {
+                digits.append(.init(rawValue: rest)!)
+            }
+            number /= 5
+        }
+        self.digits = digits
+    }
+
+    static func parse(raw: String) throws -> SnafuNumber {
+        var digits: [Digit] = []
+        for char in raw.reversed() {
+            digits.append(try Digit.parse(raw: char))
+        }
+        return .init(digits: digits)
+    }
 }
 
 // MARK: - PART 1
@@ -25,13 +109,13 @@ struct Day25: Puzzle {
 extension Day25 {
     static var partOneExpectations: [any Expectation<Input, OutputPartOne>] {
         [
-            // TODO: add expectations for part 1
+            // 4890
+            assert(expectation: "2=-1=0", fromRaw: "1=-0-2\n12111\n2=0=\n21\n2=01\n111\n20012\n112\n1=-1=\n1-12\n12\n1=\n122"),
         ]
     }
 
     static func solvePartOne(_ input: Input) async throws -> OutputPartOne {
-        // TODO: Solve part 1 :)
-        throw ExecutionError.notSolved
+        SnafuNumber(integerLiteral: input.map({ $0.intValue }).reduce(0, +)).description
     }
 }
 
